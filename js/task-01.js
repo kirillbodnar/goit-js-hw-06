da502264bc5b4f1f3add146b0f08529f3caa2cea
kirillbodnar <91017699+kirillbodnar@users.noreply.github.com>
2022-02-23 23:03:05 +0200

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -4,5 +4,5 @@ console.log(`Number of categories: ${itemElementsCounter.length}`);
 
 itemElementsCounter.forEach((element) => {
   console.log(`Category: ${element.firstElementChild.textContent}`);
-  console.log(`Elements: ${element.lastElementChild.length}`);
+  console.log(`Elements: ${element.lastElementChild.children.length}`);
 });
